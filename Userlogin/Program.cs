8c679675bfd0c1c4a03d1e7feb8f0529c2b8b779
bilalovrevan <ravanab@code.edu.az>
2021-12-10 16:25:41 +0400

    Add project files.

---
+++ Userlogin/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Userlogin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           User user = new User();
+            
+            Console.Write("username: ");
+            user.Username=Console.ReadLine();
+            Console.Write("password: ");
+            user.Password=Console.ReadLine();
+           
+            Console.WriteLine("Sisteme daxil oldunuz!");
+        }
+    }
+}
---
+++ Userlogin/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Userlogin
+{
+    internal class User
+    {
+        string username;
+        string pwd;
+       
+        public string Username
+
+        { 
+                get { return username; } set
+            {
+                if (value.Length<5)
+                {
+                    throw new Exception("minimum 5 simvol olmalidir!");
+                    
+                }
+                username = value; } }
+        public string Password
+        { get { return pwd; } set 
+            { if(value.Length<8)
+                {
+                    throw new Exception("minimum 8 simvoldan ibaret olmalidir!");
+                }
+                    this.pwd = value; } }
+             
+    }
+}
